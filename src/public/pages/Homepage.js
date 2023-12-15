889715aa6676b5d488397fa2be37f7e0d843619e
Curtis Li <curtisli7788@gmail.com>
2023-12-15 15:27:32 -0500

    parallax changes background layers, about section on front page

--- src/public/pages/Homepage.js
+++ src/public/pages/Homepage.js
@@ -7,17 +7,23 @@ function Homepage() {
     return (
         <div>
             <div className="body">
-                <Parallax pages={3}>
+                <Parallax pages={3.5}>
                     <ParallaxLayer
                         offset={0}
                         speed={1}
-                        factor={2.5}
+                        factor={2}
+                        className={styles.topLayer}
                         style={{
-                            backgroundImage: `url(${require("../../assets/stars_background.webp")})`,
+                            backgroundImage: `url(${require("../../assets/stars_background.png")})`,
                             backgroundSize: "cover",
                         }}
                     ></ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={0.5} factor={2}>
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0.5}
+                        factor={2}
+                        style={{ zIndex: 1 }}
+                    >
                         <Title text="KnowMore Hacks 2023" />
                         <p className={styles.text}>
                             Welcome to the first annual Hackathon hosted by Port
@@ -25,16 +31,35 @@ function Homepage() {
                         </p>
                     </ParallaxLayer>
                     <ParallaxLayer
-                        offset={1}
+                        offset={1.1}
+                        speed={0}
+                        style={{ zIndex: -1 }}
+                    >
+                        <h2 className={styles.fixedTitle}>About</h2>
+                        <p className={styles.fixedText}>
+                            Students in Grades 6–8 in Ontario's elementary
+                            schools are invited to participate in the Hackathon,
+                            which will provide them with an enjoyable
+                            opportunity to test their knowledge and compete with
+                            one another. It will be housed in Port Credit
+                            Secondary School and run by the Peel District School
+                            Board. The event will take over one day in May of
+                            2024. Students will have a fantastic opportunity to
+                            learn more about Peel Schools, particularly Port
+                            Credit's SciTech program, through this challenge.
+                        </p>
+                    </ParallaxLayer>
+                    <ParallaxLayer
+                        offset={1.5}
                         speed={1}
-                        factor={3}
+                        factor={4}
                         className={styles.overlapLayer}
                         style={{
-                            backgroundImage: `url(${require("../../assets/stars-nature_background.jpg")})`,
+                            backgroundImage: `url(${require("../../assets/stars-nature_background.png")})`,
                             backgroundSize: "cover",
                         }}
                     ></ParallaxLayer>
-                    <ParallaxLayer offset={1.5} speed={.5} factor={4}>
+                    <ParallaxLayer offset={2} speed={0.5} factor={4}>
                         <h2 className={styles.title}>Sponsors</h2>
                     </ParallaxLayer>
                 </Parallax>
