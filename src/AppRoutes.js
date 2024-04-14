7c6c84cbb17a6812923185697cf014cc6f8ec777
lbcmk <30442978+lbcmk@users.noreply.github.com>
2024-04-13 20:24:45 -0400

    feat: Terms of Service Page

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -6,9 +6,9 @@ import Navbar from "./public/modules/navbar";
 import Homepage from "./public/pages/Homepage";
 import FAQ from "./public/pages/FAQ";
 import Dashboard from './auth/pages/dashboard'
-import TermsOfService from './auth/pages/terms'
 import { checkAuth, pb } from './auth' 
 import DashboardNavbar from "./auth/modules/navbar";
+import TermsOfServicePage from "./auth/pages/termsPage";
 
 function PrivateRoute({ children }) {
     checkAuth()
@@ -42,8 +42,16 @@ const AppRoutes = () => (
                     element={
                         <PrivateRoute>
                             <DashboardNavbar />
-                            <TermsOfService />
                             <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+                <Route 
+                    path="tos"
+                    element={
+                        <PrivateRoute>
+                            <DashboardNavbar />
+                            <TermsOfServicePage />
                         </PrivateRoute>
                     }
                 />
---
+++ src/auth/pages/termsPage.js
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import styles from './terms.module.css'
+
+function TermsOfServicePage() {
+	return (
+		<>
+			<div className={styles.main}>
+				<div className={styles.content}>
+					<div className={styles.title}>
+						HACKATHON TERMS
+					</div>
+					<ol className={styles.rules}>
+						<li>
+							Respect and Appropriateness: By participating in this hackathon, I agree to treat all other participants, volunteers, organizers, judges, mentors, and guests with respect at all times. 
+							This includes refraining from any form of bullying, harassment, or discrimination.
+						</li>
+						<li>
+							Follow the Peel District School Board's Code of Conduct (<a href="https://www.peelschools.org/code-of-conduct">https://www.peelschools.org/code-of-conduct</a>)
+						</li>
+						<li>
+							Reporting Inappropriate Behavior: If I witness or become aware of any inappropriate behavior by another participant, 
+							I will report it to the hackathon organizers as soon as possible. 
+							Managers/Organizers reserve the right to take any action as they see fit and to remove any participant who violates these terms of service, 
+							and the ability to contact their school over any inappropriate behaviour.
+						</li>
+						<li>
+							Reporting Inappropriate Behavior: If I witness or become aware of any inappropriate behavior by another participant, 
+							I will report it to the hackathon organizers as soon as possible. 
+							Managers/Organizers reserve the right to take any action as they see fit and to remove any participant who violates these terms of service, 
+							and the ability to contact their school over any inappropriate behaviour.
+						</li>
+						<li>
+							Do not use any personal information when creating a team name
+						</li>
+						<li>
+							A form will be sent to your email address that your parents must sign and bring to the hackathon
+						</li>
+						<li>
+							If your parents request any information about the hackathon please have them contact the organizer at <a href="mailto:p0076296@pdsb.net">p0076296@pdsb.net</a>
+						</li>
+					</ol>
+					<div className={styles.acceptHolder}>
+						<Link to="/user/dashboard" className={styles.accept + " " + styles.acceptLink}>Understood</Link>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default TermsOfServicePage;
